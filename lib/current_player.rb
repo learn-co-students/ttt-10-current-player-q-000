b9d899db99f421d9d4a5c652c9c3d279fe468f50
Shana L Moore <shana.lavina.moore@gmail.com>
2015-10-12 13:56:40 -0700

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,21 @@ def turn_count(board)
 end
 
 
+# def current_player(board)
+#   if turn_count(board) % 2 == 0
+#     "X"
+#   else
+#     "O"
+#   end
+# end
+
+# def current_player(board)
+#   if turn_count(board).even?
+#     "X"
+#   else
+#     "O"
+# end
+
 def current_player(board)
   turn_count(board).even? ? "X" : "O"
 end
-
