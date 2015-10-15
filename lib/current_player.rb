8f8206817c1980259340ea706f4499a4cbde956e
Ahertig <alyssa.hertig@gmail.com>
2015-10-15 20:19:55 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |position|
+    if position == "" || position == " " || position == nil
+      counter += 0
+    else
+      counter += 1
+    end
+  end
+  counter
+end
+
