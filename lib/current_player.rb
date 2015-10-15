c89e32981bd397959ee7ae4d033bd6db9b99afd6
playto <hacker4010@gmail.com>
2015-10-15 00:59:25 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  counter = 0
+  board.each do |position|
+    if position == "X" || position == "O" || position == "x" || position == "o"
+      counter += 1
+    else
+    end
+  end
+    counter
+end
+
+def current_player(board)
+  !turn_count(board).even? ? "X" : "O"
+end
