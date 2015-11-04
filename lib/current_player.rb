9d533131705d92769991a7d920613b1e2450c56c
tyleresmith <oats000@hotmail.com>
2015-11-04 23:40:16 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+
+def turn_count(board)
+  counter = 0
+  board.each do |position|
+    if position == "X" || position == "O"
+      counter +=1
+    else
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+current_player(board)
