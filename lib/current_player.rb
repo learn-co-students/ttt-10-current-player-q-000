dc360bbb0de7aa699fab77f947b2be76073d006f
LKWLaLa <lkwlala@gmail.com>
2015-11-06 01:21:44 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+board = ["X", "O", " ", "X"]
+
+def turn_count(board)
+  count = 0
+  board.each do |position|
+    if position != "" && position != " " && position != nil
+      count +=1
+    end
+  end
+  return count
+end
+
+
+def current_player(board)
+  counted = turn_count(board)
+  (counted == 0 ||counted % 2 == 0)? "X" : "O"
+end
+
+
