5cd97b2a957a5e764c8357c12585347715bfca13
Scotty Runyan <runyans7@gmail.com>
2015-11-08 16:02:09 -0500

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  turn_count = 0
+
+  board.each do |position|
+    if position == "X" || position == "O"
+      turn_count += 1
+    end
+
+  end
+  turn_count
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
