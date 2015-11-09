714d3bd65151f23bb17c8c7c8ecf97a4f91da678
shmulim <shmuel.markel@gmail.com>
2015-11-09 01:07:38 -0800

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,6 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    return "X"  
-  else
-    return "O"
+  turn_count(board) % 2 == 0 ? return "X" : return "O"
   end
 end
