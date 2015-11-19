4c5680b6992870438480a1a2301e0309ad857144
dantepicasso <dantepicasso@gmail.com>
2015-11-19 05:53:14 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
 turns = 0
-  board.each do |token|
-    if token == "X" || token == "O"
+  board.each do |pick|
+    if pick == "X" || pick == "O"
       turns += 1
     end
   end
