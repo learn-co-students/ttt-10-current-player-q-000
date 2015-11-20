b99fa3994cc68eac4ed935cd3c266b466e4f19ce
yoshikot <ytamaoki@gmail.com>
2015-11-20 00:09:20 -0500

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,20 +8,27 @@ count = 0
   count
 end
 
-def current_player(board)
-  x_count = 0
-  o_count = 0
-  board.each do |square|
-    if square == "X"
-      x_count += 1
-    elsif square == "O"
-      o_count += 1
-    end
-  end
-  if x_count > o_count 
-    "O"
-  elsif x_count = o_count
-    "X"
-  end 
-end
+# def current_player(board)
+#   x_count = 0
+#   o_count = 0
+#   board.each do |square|
+#     if square == "X"
+#       x_count += 1
+#     elsif square == "O"
+#       o_count += 1
+#     end
+#   end
+#   if x_count > o_count 
+#     "O"
+#   elsif x_count = o_count
+#     "X"
+#   end 
+# end
 
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  elsif turn_count(board) % 2 != 0
+    "O"
+  end
+end
