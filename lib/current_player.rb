bde30245103541c992bb343884c1f1430e6c79e6
yoshikot <ytamaoki@gmail.com>
2015-11-20 00:01:04 -0500

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,27 @@
+def turn_count(board)
+count = 0
+  board.each do|square| 
+    if square == "X" || square == "O"
+      count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  x_count = 0
+  o_count = 0
+  board.each do |square|
+    if square == "X"
+      x_count += 1
+    elsif square == "O"
+      o_count += 1
+    end
+  end
+  if x_count > o_count 
+    "O"
+  elsif x_count = o_count
+    "X"
+  end 
+end
+
