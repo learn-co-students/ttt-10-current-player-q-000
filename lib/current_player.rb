6617d4a5c960170eeeb4c904ea58c366120005c7
jessiegibson <jessie@thirstynyc.com>
2015-11-23 08:35:24 -0700

    Created the methods #turn_cound and #current_player (using and if/else statement

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+def turn_count(board)
+  counter = 0
+  board.each do |count|
+    if count == "X" || count == "O"
+      counter +=1
+    end
+  end
+return counter
+end
+
+def current_player(board)
+
+  if turn_count(board)  == 1
+    return "O"
+  elsif turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+
+end #Ends the method
+
