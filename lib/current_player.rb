23327631b2978b190d4123c6c8af2c79b6d003ef
bizworld <bizworld@outlook.com>
2015-11-24 15:56:54 -0500

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  counter = 0
+  board.each do |position|
+    if position == "X" || position == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board) 
+  turn = ""
+  if turn_count(board) % 2 == 0 
+    turn = "X" # turn's test should be reversed 
+  else
+    turn = "O"
+  end 
+  turn
+end
