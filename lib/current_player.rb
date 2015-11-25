1ea9647db08c43b40235acd38600c5b65291ac62
corya0687 <corya0687@gmail.com>
2015-11-25 01:49:17 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+board = Array.new(9," ")
+def turn_count(board)
+  occupied_positions = 0
+  board.each do |countocc|
+    if countocc == "X" || countocc == "O"
+      occupied_positions += 1
+    end
+  end
+  return occupied_positions
+end
+
+def current_player(board)
+
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
