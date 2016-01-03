5d39bf4d84cbfe17f7e3b18e86fbccc266956f43
rambllah <adam.bramhall@gmail.com>
2016-01-03 01:40:14 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,11 +10,7 @@ end
   turns_taken
 end
 
+
 def current_player(board)
-  
-  if turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
