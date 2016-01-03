cbf8f464a411c08e2707fa77e00cac50a6159b50
fossbender <fossbender@gmail.com>
2016-01-03 21:36:02 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  turns_played = 0
+  board.each do | turn |
+   if turn != "" && turn!= " "
+     turns_played += 1
+   end
+  end
+  return turns_played
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+  #if turn_count(board) % 2 == 0
+  #   return
+  # else
+  #   return "O"
+  # end
+end
