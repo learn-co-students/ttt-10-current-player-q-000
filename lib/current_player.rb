1a2312f7c1f29b59d7a3211482cfc70d2e23c27e
tal32123 <tal32123@gmail.com>
2016-01-10 01:07:47 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+# board = [" ", " ", " ", " ", "X", "O", "X", " ", " "]
+def turn_count(board)
+  turnnumber = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+     turnnumber += 1
+
+    end
+  end
+  return turnnumber
+end
+
+def current_player(board)
+  if turn_count(board).to_i%2==0
+    player="X"
+  else
+    player="O"
+  end
+  return player
+end
+
