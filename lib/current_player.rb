8ca62b066479d6173ad20446bb32e2666c6d9994
VanHeezy <stephen.vanover@usaa.com>
2016-01-11 21:41:17 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def turn_count(board)
+counter = 0
+board.each do |count|
+  if count == "X" || count == "O"
+    counter += 1
+end
+end
+  counter
+end
+
+def current_player(board)
+if turn_count(board) % 2 == 0
+  "X"
+else
+  "O"
+end
+end
+
+
+
