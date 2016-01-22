919ca1cf0b8844927afec4d3ef1cc22016c4f31f
the-widget <brennenawana108ny@gmail.com>
2016-01-22 13:02:28 -0500

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,5 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-
 def turn_count(board)
   count = 0
   board.each do |position|
@@ -15,7 +14,7 @@ def current_player(board)
   turn_count(board).even? ? "X" : "O"
 end
 
-
+# *Example from Ruby-Doc*
 #input_type = gets =~ /hello/i ? "greeting" : "other"
 #Is the same as this if expression:
 
