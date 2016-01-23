32535738febaba96d961915c1965787645352461
andytouch93 <andy.touch.93@icloud.com>
2016-01-23 02:03:07 -0600

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def turn_count(board)
+  counter = 0
+  board.each do |board|
+    if "#{board}" == "X" || "#{board}" == "O" 
+      counter +=1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  else turn_count(board).odd?
+    "O"
+  end
+end
