38b4a6645d7517c78ab0766cf9baca0e80b31c4d
jbok4 <jbok4@aol.com>
2016-02-06 16:34:14 -0500

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,17 @@
 def turn_count(board)
-  #counter = 0
-  board.each_with_object(Hash.new(0)) { |word,counts| counts[words] += 1 }
-  #if board.grep(X).size >= 0 || board.grep(O).size > 0
-    #counter += 1
+    board.count("X") + board.count("O")
   end
-  #counter
-#end
+
 
 
 def current_player(board)
-  counter.even ? "X" : "O"
+  if turn_count(board).even? 
+    "X"
+  else
+    "O"
 end
+end
+
+
+
+#board.grep("X").size >= 0 || board.grep("O").size > 0
