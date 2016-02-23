02fc8456eda59ed3960853cd602355770b1971f8
asackofwheat <asackofwheat@gmail.com>
2016-02-22 19:38:00 -0500

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board)
+  counter = 0
+  board.each do |position|
+    if position == "X"
+      counter += 1
+    elsif position == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    return "X"
+  elsif turn_count(board).odd?
+    return "O"
+  end
+end
+
