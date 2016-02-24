363127ec6dc2ff8896a6948a2cfbbcb3c883dcf3
mcarlson33 <mcarlson@bitmosphere.com>
2016-02-24 13:40:01 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  count = 0
+  board.each { |item|
+    if item == 'X' || item == 'O'
+      count += 1
+    end
+    }
+  return count
+end
+
+def current_player(board)
+  count = turn_count(board)
+  if count % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
