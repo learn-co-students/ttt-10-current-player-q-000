daf2a31887ef1ca26cc600cb8575802b0095dfad
Green-stripes <kavericb@hotmail.com>
2016-04-05 12:39:06 +0100

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -23,10 +23,6 @@ return counter
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0 
-    return "X"
-  else
-    return "O"
-  end
+  turn_count(board) % 2 == 0 ? (return "X") : (return "O")
 end
 
