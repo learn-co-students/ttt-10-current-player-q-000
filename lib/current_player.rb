6c1e332e503980174b6c3dff0a12eef394a8a5d0
Green-stripes <kavericb@hotmail.com>
2016-04-05 12:32:58 +0100

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,32 @@
+
+
+#board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+
+#return number of turns that have been played
+#count_x = 0
+#count_o = 0
+
+def turn_count(board)
+ counter = 0
+ board.each do |q| 
+  if q != " "
+    counter += 1
+ #if q == "X" 
+  #count_x = count_x + 1 
+#elsif q == "O"
+  #count_o = count_o + 1 
+end
+end
+#return count_x
+#return count_o
+return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    return "X"
+  else
+    return "O"
+  end
+end
+
