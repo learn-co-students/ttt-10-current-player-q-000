033e29308f036db49a5d0a6d46f170ccedb87405
theRingleman <sam.ringleman@gmail.com>
2016-04-12 13:46:43 -0600

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,14 +9,6 @@ def turn_count (board)
 end
 
 def current_player (board)
-  if turn_count(board) == 0 || turn_count(board) % 2 == 0
-    player = "X"
-  else
-    player = "O"
-  end
+  player = turn_count(board) == 0 || turn_count(board) % 2 == 0 ? "X" : "O"
   player
 end
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-puts current_player(board)
