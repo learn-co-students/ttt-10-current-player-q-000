3ec2f7e3f4175f903848aa8d67a44f11d7f4cbd6
beccagotch <becca.gotch@archiv8.com>
2016-04-30 16:16:05 -0400

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,35 +1,17 @@
  def turn_count(board)
-  #9 - board.count(" ")
-  turns = ["1","2"]
-  counter = 0
-   turns.each do |turn|
-puts "#{turn}"
-counter += 1
-end
-  #turns = ["1","2","3","4","5","6","7","8","9"]
-  #counter = 1
-  #turns.each do |turn|
-  #puts "#{turn}"
-  #counter +=1
-  #end
-end
+  # board.count{|token| token == "X" || token == "O"}
 
- def current_player(board,turn_count)
- if turn_count(board) % 2 == 0
-  puts "X"
-  else
-  puts "O"
-end
+  turns = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      turns += 1
+    end
+  end
+  turns
+
 end
 
 
-
-#def turn_count(board)
-#counter = 0
-##puts "#{counter}"
-#counter += 1
-#end
-#end
-  # if empty spaces on board are % 2, the "O"
-  #elsif empy spaces on 
-
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
