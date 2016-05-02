caa8896d94402f25ae77898c8c681cb7fc2e9bcd
alice <github email address>
2016-05-01 22:20:06 -0400

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,10 +8,14 @@ def turn_count(board)
   round
 end
 
+#def current_player(board)
+#  if turn_count(board) % 2 == 0
+#    "X"
+#  else
+#    "O"
+#  end
+#end
+
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    "X"
-  else
-    "O"
-  end
+    turn_count(board) % 2 == 0 ? "X" : "O"
 end
