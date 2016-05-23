1d3f24e62e97a7404d25d2f2dfdcdeb631db5fb6
James White <aprsr1@consolidated.net>
2016-05-23 12:36:21 -0500

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,4 @@
+#is it even or odd
 def turn_count(board)
   count = 0
   board.each do |turn|
@@ -7,9 +8,7 @@ def turn_count(board)
   end
   count
 end
-#if object.method?
-
+#Fin out what player goes next
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
-
 end
