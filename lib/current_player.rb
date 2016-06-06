36bc960fa2489fb98cf499d2ad312c188deff7d0
Josue Magallanes <josue_m_23@hotmail.com>
2016-06-06 21:51:41 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,26 +4,18 @@ board = ["X", "O", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
 counter = 0
 board.each do |filled|
-if filled == "X" || filled == "O"
-counter += 1
-end
-end
+    if filled == "X" || filled == "O"
+      counter += 1
+    end
+  end
 return counter
 end
 
 
+
+
 def current_player(board)
-player = ""
-if turn_count(board) % 2 == 0
-  puts "Should be an even number"
-  puts "X"
-  player = "X"
-  #board[turn_count]
-else
-  puts "Should be an odd number"
-  puts "O"
-  player = "O"
-end
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
 #Testing the method
