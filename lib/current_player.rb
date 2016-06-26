202893d73ec24388371de5a540d7dae0cfaf814a
Scott Dixon <dixon.scottr@gmail.com>
2016-06-26 22:16:57 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+
+
+def turn_count(board)
+  counter = 0
+  board.each do |x|
+    if x != " "
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
