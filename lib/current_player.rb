448995775c237520611a9f60322d15214976b8c6
smithwebtek <brad@smithwebtek.com>
2017-09-05 16:52:02 -0400

    refactore

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,21 +1,18 @@
 def turn_count(board)
-    counter = 0
-    board.each do |value| 
-      if value == ("X" || "O")
-        counter += 1
-      elsif value == ("O")
-        counter += 1
-      else 
-      end
+	counter = 0
+	board.each do |value| 
+		if value != " "
+			counter += 1
     end
-    counter
-  end
+	end
+	counter
+end
 
 def current_player(board)
-    turn = 0
+  turn = 0
   if turn_count(board) % 2 == 0
     turn = "X"
-  else turn_count(board) % 2 != 0
-    turn = "O"
+  else
+  turn = "O"
   end
 end
